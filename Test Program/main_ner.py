4937d87bc6b445a8964f384c57ea235878abf1c9
geraldzakwan <geraldi.dzakwan@gmail.com>
2017-06-15 14:32:10 +0900

    done handling consecutive trees

--- Test Program/main_ner.py
+++ Test Program/main_ner.py
@@ -33,7 +33,7 @@ def text_ner_tagging(tagged_sentences):
     chunked_sentences = [nltk.ne_chunk(word, binary=False) for word in tagged_sentences]
 
     # Debugging
-    # print(chunked_sentences)
+    print(chunked_sentences)
 
     # This will be the output of NER processing
     processed_list = []
@@ -47,24 +47,56 @@ def text_ner_tagging(tagged_sentences):
         # Debugging
         # print(inside_chunked_sentences[iterator_1])
 
+        whole_word = ""
+        whole_label = "None"
+
         # Check if it contains named entity label
         if (type(inside_chunked_sentences[iterator_1]) == nltk.tree.Tree):
             # Later check for case there is 'of' between named entity (between Tree)
-            # e.g. United States of America
-            whole_word = ""
+            # e.g. United States of
+            is_tree = False
             while(type(inside_chunked_sentences[iterator_1]) == nltk.tree.Tree):
                 # If it is a named entity, add the right label/class to the tuple
-                # Belom di gabung, harusnya digabung jadi satu item
+                # Group all consecutive same named entity
+                # Check also if a tree has more than one item, e.g. : Tree('ORGANIZATION', [('Real', 'NNP'), ('Madrid', 'NNP')])
+
+                temp_word = inside_chunked_sentences[iterator_1][0][0]
+                first = True
+                for word_tuple in inside_chunked_sentences[iterator_1]:
+                    if(not first):
+                        temp_word += " "
+                        temp_word += word_tuple[0]
+                    first = False
+
+                whole_word += temp_word
+                whole_word += " "
+
+                whole_label = inside_chunked_sentences[iterator_1].label()
+
                 # processed_list.append([inside_chunked_sentences[iterator_1][0][0], inside_chunked_sentences[iterator_1].label()])
-                whole_word += inside_chunked_sentences[iterator_1][0][0]
+                # processed_list.append([temp_word, inside_chunked_sentences[iterator_1].label()])
+
+                # Debugging
+                # print(inside_chunked_sentences[iterator_1][0][0], inside_chunked_sentences[iterator_1].label())
+
+                # whole_word += inside_chunked_sentences[iterator_1][0][0]
 
                 # Debugging
                 # print('To NER : ', inside_chunked_sentences[iterator_1][0][0])
 
+                is_tree = True
+
                 iterator_1 = iterator_1 + 1
 
-        print(inside_chunked_sentences[iterator_1-1]);
-        processed_list.append([whole_word, inside_chunked_sentences[iterator_1-1].label()])
+
+        if (is_tree):
+            processed_list.append([whole_word, whole_label])
+
+        # Debugging
+        # print('DELIMITER CHUNK')
+
+        # print(inside_chunked_sentences[iterator_1-1]);
+        # processed_list.append([whole_word, inside_chunked_sentences[iterator_1-1].label()])
 
         # If it is not a named entity, add None label to the tuple
         processed_list.append([inside_chunked_sentences[iterator_1][0], None])
@@ -93,8 +125,9 @@ if __name__ == "__main__":
     if (text == 'default'):
         # text = "Barack Obama is a great person in United States of America and Great Britain. He is truly our president."
         # Bugs here
-        text = "Cristiano Ronaldo is a decent footballer both in Spain (Real Madrid) and United Kingdom (Manchester United). He is truly a masterpiece."
-        # text = "Cristiano Ronaldo is a decent footballer both in Real Madrid, Spain and Manchester United, Great Britain. He is truly a masterpiece."
+        # text = "Cristiano Ronaldo is a decent footballer both in Spain (Real Madrid) and United Kingdom (Manchester United). He is truly a masterpiece."
+        text = "Cristiano Ronaldo is a decent footballer both in Real Madrid, Spain and Manchester United, Great Britain. He is truly a masterpiece."
+        # text = "Cristiano Ronaldo is a decent footballer both in Real Madrid."
 
     tagged_sentences = text_preprocessing(text)
 
