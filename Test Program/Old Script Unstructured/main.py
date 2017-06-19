ed6839889fb4851bdfc5f798fd7060521554529d
geraldzakwan <geraldi_dzakwan@yahoo.co.id>
2017-06-20 00:13:22 +0900

    restructure module

--- Test Program/Old Script Unstructured/main.py
+++ Test Program/Old Script Unstructured/main.py
@@ -1,5 +1,6 @@
-import ne_chunker
-import corpus
+import main_ner_2
+import nltk_ner_trainer
+import access_corpus
 from nltk import pos_tag, word_tokenize
 from nltk.chunk import conlltags2tree, tree2conlltags
 
@@ -7,7 +8,7 @@ from nltk.chunk import conlltags2tree, tree2conlltags
 corpus_root = 'gmb-2.2.0'
 mode = '--core'
 
-data = corpus.read_corpus(corpus_root, mode)
+data = access_corpus.read_corpus(corpus_root, mode)
 
 training_samples = data[:int(len(data) * 0.9)]
 test_samples = data[int(len(data) * 0.9):]
@@ -15,7 +16,7 @@ test_samples = data[int(len(data) * 0.9):]
 print "#training samples = %s" % len(training_samples)    # training samples = 55809
 print "#test samples = %s" % len(test_samples)                # test samples = 6201
 
-chunker = ne_chunker.NamedEntityChunker(training_samples[:55809])
+chunker = nltk_ner_trainer.NamedEntityChunker(training_samples[:2000])
 # text = "Cristiano Ronaldo is a decent footballer both in Real Madrid, Spain and Manchester United, United Kingdom. He is truly a masterpiece."
 text = "Geraldi Dzakwan wakes up at 7 am every morning."
 print chunker.parse(pos_tag(word_tokenize(text)))
@@ -23,5 +24,5 @@ print chunker.parse(pos_tag(word_tokenize(text)))
 score = chunker.evaluate([conlltags2tree([(w, t, iob) for (w, t), iob in iobs]) for iobs in test_samples[:500]])
 
 # Debugging
-print score.accuracy()
 # 0.931132334092
+print score.accuracy()
