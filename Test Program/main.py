bde3b2f0d378c1fc15da59613f4376ac1ff2c8d5
geraldzakwan <geraldi_dzakwan@yahoo.co.id>
2017-06-20 16:11:52 +0900

    modify gitignore for model file

--- Test Program/main.py
+++ Test Program/main.py
@@ -1,6 +1,7 @@
+import sys
 import perceptron
 
 if __name__ == "__main__":
-    classifier = perceptron.train_perceptron(10000)
-    perceptron.save_perceptron(classifier, '10000_train_data')
+    classifier = perceptron.train_perceptron(int(sys.argv[1]))
+    perceptron.save_perceptron(classifier, sys.argv[1] + '_train_data')
     result = perceptron.classify(classifier, 'default')
